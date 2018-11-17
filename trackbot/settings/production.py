f16b17273643cb88921ebefc5d77c529f96f625e
Farhan <farhankasmani956@gmail.com>
2018-11-17 20:34:46 +0530

    done

--- trackbot/settings/production.py
+++ trackbot/settings/production.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'farhankasmani956@gmail.com'
-EMAIL_HOST_PASSWORD = 'FarhanK29081998'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Python trackbot <farhankasmani956@gmail.com>'
