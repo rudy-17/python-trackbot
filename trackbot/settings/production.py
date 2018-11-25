052136a954ac670121b9b1c4e6564a181cacd01c
Farhan <farhankasmani956@gmail.com>
2018-11-25 16:01:36 +0530

    Removed whitenoise settings entry

--- trackbot/settings/production.py
+++ trackbot/settings/production.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_my_proj"),
