0af1bae0f3fc98f50c72067e6a1e8edbe7608c09
Farhan <farhankasmani956@gmail.com>
2018-11-25 15:29:04 +0530

    Done

--- trackbot/settings/production.py
+++ trackbot/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '0.0.0.0:5000']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'farhankasmani956@gmail.com'
